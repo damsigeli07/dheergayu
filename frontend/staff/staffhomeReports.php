a7ac1ddfeeaff306229101ed37c005ee072a925e
Amashi Vithanage <amashiimayav@gmail.com>
2025-09-01 10:23:32 +0530

    added profile tab

--- frontend/staff/staffhomeReports.php
+++ frontend/staff/staffhomeReports.php
@@ -51,9 +51,10 @@ if (!empty($searchQuery)) {
                 <div class="user-icon" id="user-icon">👤</div>
                 <span class="user-role">Staff</span>
                 <!-- Dropdown -->
-                <div class="user-dropdown" id="user-dropdown">
-                    <a href="../patient/login.php" class="logout-btn">Logout</a>
-                </div>
+        <div class="user-dropdown" id="user-dropdown">
+            <a href="pharmacistprofile.php" class="profile-btn">Profile</a>
+            <a href="../patient/login.php" class="logout-btn">Logout</a>
+        </div>
             </div> 
         </div>
     </header>
