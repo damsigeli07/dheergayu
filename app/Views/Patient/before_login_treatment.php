bee3b6e2294266a2ec4d0cfa35c9a61f844ba670
Damsi <damsijayawarna7@gmail.com>
2025-10-18 12:55:12 +0530

    Changed treatment types

--- app/Views/Patient/before_login_treatment.php
+++ app/Views/Patient/before_login_treatment.php
@@ -34,9 +34,14 @@
             <div class="treatment-card">
                 <img src="/dheergayu/public/assets/images/Patient/asthma.png" alt="asthma" class="treatment-image">
                 <div class="treatment-content">
-                    <h3 class="treatment-name">Asthma</h3>
+                    <h3 class="treatment-name">Nasya Karma</h3>
                     <p class="treatment-description">Traditional full-body massage using warm herbal oils</p>
                     <div class="key-benefits">
+                        <h4 class="benefits-title">Condition</h4>
+                        <ul class="benefits-list">
+                            <li>Asthma</li>
+                            <li>ENT Disorders</li>
+                        </ul>
                         <h4 class="benefits-title">Key Benefits</h4>
                         <ul class="benefits-list">
                             <li>Reduces stress and anxiety</li>
@@ -46,27 +51,17 @@
                 </div>
             </div>
 
-            <div class="treatment-card">
-                <img src="/dheergayu/public/assets/images/Patient/diabetes.jpg" alt="diabetes" class="treatment-image">
-                <div class="treatment-content">
-                    <h3 class="treatment-name">Diabetes</h3>
-                    <p class="treatment-description">Continuous flow of warm oil on the forehead</p>
-                    <div class="key-benefits">
-                        <h4 class="benefits-title">Key Benefits</h4>
-                        <ul class="benefits-list">
-                            <li>Calms the mind</li>
-                            <li>Reduces stress</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <div class="treatment-card">
+                        <div class="treatment-card">
                 <img src="/dheergayu/public/assets/images/Patient/skin_diseases.jpg" alt="skin diseases" class="treatment-image">
                 <div class="treatment-content">
-                    <h3 class="treatment-name">Skin Diseases</h3>
+                    <h3 class="treatment-name">Panchakarma Detox</h3>
                     <p class="treatment-description">Complete detoxification and rejuvenation therapy</p>
                     <div class="key-benefits">
+                        <h4 class="benefits-title">Condition</h4>
+                        <ul class="benefits-list">
+                            <li>Diabetes</li>
+                            <li>Skin Diseases</li>
+                        </ul>
                         <h4 class="benefits-title">Key Benefits</h4>
                         <ul class="benefits-list">
                             <li>Deep detoxification</li>
@@ -79,9 +74,13 @@
             <div class="treatment-card">
                 <img src="/dheergayu/public/assets/images/Patient/respiratory_disorders.jpg" alt="respiratory disorders" class="treatment-image">
                 <div class="treatment-content">
-                    <h3 class="treatment-name">Respiratory Disorders</h3>
+                    <h3 class="treatment-name">Vashpa Sweda</h3>
                     <p class="treatment-description">Nasal administration of herbal oils</p>
                     <div class="key-benefits">
+                        <h4 class="benefits-title">Condition</h4>
+                        <ul class="benefits-list">
+                            <li>Respiratory Disorders</li>
+                        </ul>
                         <h4 class="benefits-title">Key Benefits</h4>
                         <ul class="benefits-list">
                             <li>Clears nasal passages</li>
@@ -94,9 +93,13 @@
             <div class="treatment-card">
                 <img src="/dheergayu/public/assets/images/Patient/arthritis.jpg" alt="arthritis" class="treatment-image">
                 <div class="treatment-content">
-                    <h3 class="treatment-name">Arthritis</h3>
+                    <h3 class="treatment-name">Elakizhi</h3>
                     <p class="treatment-description">Specialized treatment for lower back pain</p>
                     <div class="key-benefits">
+                        <h4 class="benefits-title">Condition</h4>
+                        <ul class="benefits-list">
+                            <li>Arthritis</li>
+                        </ul>
                         <h4 class="benefits-title">Key Benefits</h4>
                         <ul class="benefits-list">
                             <li>Reduces back pain</li>
@@ -107,26 +110,16 @@
             </div>
 
             <div class="treatment-card">
-                <img src="/dheergayu/public/assets/images/Patient/ENT_disorders.jpg" alt="ENT disorders" class="treatment-image">
-                <div class="treatment-content">
-                    <h3 class="treatment-name">ENT Disorders</h3>
-                    <p class="treatment-description">Herbal powder massage for body toning</p>
-                    <div class="key-benefits">
-                        <h4 class="benefits-title">Key Benefits</h4>
-                        <ul class="benefits-list">
-                            <li>Improves skin texture</li>
-                            <li>Helps with weight management</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            <div class="treatment-card">
                 <img src="/dheergayu/public/assets/images/Patient/paralysis.jpg" alt="paralysis" class="treatment-image">
                 <div class="treatment-content">
-                    <h3 class="treatment-name">Neurological Diseases and Paralysis</h3>
+                    <h3 class="treatment-name">Basti</h3>
                     <p class="treatment-description">Traditional Ayurvedic foot massage</p>
                     <div class="key-benefits">
+                        <h4 class="benefits-title">Condition</h4>
+                        <ul class="benefits-list">
+                            <li>Neurological Diseases and Paralysis</li>
+                            <li>Osteoporosis</li>
+                        </ul>
                         <h4 class="benefits-title">Key Benefits</h4>
                         <ul class="benefits-list">
                             <li>Relieves foot fatigue</li>
@@ -139,9 +132,13 @@
             <div class="treatment-card">
                 <img src="/dheergayu/public/assets/images/Patient/bone_disorders.png" alt="bone disorders" class="treatment-image">
                 <div class="treatment-content">
-                    <h3 class="treatment-name">Dislocation Features of Joints & Bones</h3>
+                    <h3 class="treatment-name">Abhyanga</h3>
                     <p class="treatment-description">Energy point therapy</p>
                     <div class="key-benefits">
+                        <h4 class="benefits-title">Condition</h4>
+                        <ul class="benefits-list">
+                            <li>Dislocation Features of Joints & Bones</li>
+                        </ul>
                         <h4 class="benefits-title">Key Benefits</h4>
                         <ul class="benefits-list">
                             <li>Relieves foot fatigue</li>
@@ -152,25 +149,15 @@
             </div>
 
             <div class="treatment-card">
-                <img src="/dheergayu/public/assets/images/Patient/osteoporosis.png" alt="osteoporosis" class="treatment-image">
-                <div class="treatment-content">
-                    <h3 class="treatment-name">Osteoporosis</h3>
-                    <p class="treatment-description">Energy point therapy</p>
-                    <div class="key-benefits">
-                        <h4 class="benefits-title">Key Benefits</h4>
-                        <ul class="benefits-list">
-                            <li>Relieves foot fatigue</li>
-                            <li>Improves circulation</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="treatment-card">
                 <img src="/dheergayu/public/assets/images/Patient/Stress.png">
                 <div class="treatment-content">
-                    <h3 class="treatment-name">Anxiety, Stress and Depression</h3>
+                    <h3 class="treatment-name">Shirodhara</h3>
                     <p class="treatment-description">Energy point therapy</p>
                     <div class="key-benefits">
+                        <h4 class="benefits-title">Condition</h4>
+                        <ul class="benefits-list">
+                            <li>Anxiety, Stress and Depression</li>
+                        </ul>
                         <h4 class="benefits-title">Key Benefits</h4>
                         <ul class="benefits-list">
                             <li>Relieves foot fatigue</li>
@@ -182,9 +169,13 @@
             <div class="treatment-card">
                 <img src="/dheergayu/public/assets/images/Patient/cholesterol.png" alt="cholesterol" class="treatment-image">
                 <div class="treatment-content">
-                    <h3 class="treatment-name">Cholesterol</h3>
+                    <h3 class="treatment-name">Udwarthana</h3>
                     <p class="treatment-description">Energy point therapy</p>
                     <div class="key-benefits">
+                        <h4 class="benefits-title">Condition</h4>
+                        <ul class="benefits-list">
+                            <li>Cholesterol</li>
+                        </ul>
                         <h4 class="benefits-title">Key Benefits</h4>
                         <ul class="benefits-list">
                             <li>Relieves foot fatigue</li>
