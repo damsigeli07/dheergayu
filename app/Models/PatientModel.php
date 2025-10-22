4c0a724b0e7682925220dac485a938d599654e58
Damsi Jayawarna <damsijayawarna7@gmail.com>
2025-10-22 12:29:51 +0530

    Update model page

--- app/Models/PatientModel.php
+++ app/Models/PatientModel.php
@@ -18,6 +18,8 @@ class PatientModel {
         return $result;
     }
 
+
+    
     // Create initial patient profile
     public function createProfile($user_id, $email, $first_name = '', $last_name = '') {
         $stmt = $this->conn->prepare("
